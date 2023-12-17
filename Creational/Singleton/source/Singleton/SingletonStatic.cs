86281b5f038011b73709f6b9811ca6ef1e4ba624
longjun xu <xulongjun.fr@gmail.com>
2023-12-17 22:15:42 +0100

    feat : add commentatire

--- Creational/Singleton/source/Singleton/SingletonStatic.cs
+++ Creational/Singleton/source/Singleton/SingletonStatic.cs
@@ -12,8 +12,11 @@ namespace Singleton
     {
         private static readonly SingletonStatic instance = new();
 
-        //Static constructors in C# are specified to execute only once per Application Domain, and they are thread-safe. This is a simpler approach but with a slight difference in behavior compared to Lazy<T>: the instance is created when the class is first loaded, not the first time it is accessed.
-        //In this implementation, the static constructor doesn't actually do anything, but it's presence ensures the static fields are initialized in a thread-safe manner.
+        // Static constructors in C# are specified to execute only once per Application Domain, and they are thread-safe. This is a simpler approach but with a slight difference in behavior compared to Lazy<T>: the instance is created when the class is first loaded, not the first time it is accessed.
+        // In this implementation, the static constructor doesn't actually do anything, but it's presence ensures the static fields are initialized in a thread-safe manner.
+        // Static members are 'eagerly initialized', that is, 
+        // immediately when class is loaded for the first time.
+        // .NET guarantees thread safety for static initialization
         static SingletonStatic() { }
         private SingletonStatic() { }
 
