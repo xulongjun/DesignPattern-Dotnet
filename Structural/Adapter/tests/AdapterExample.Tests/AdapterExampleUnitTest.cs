a9f97de199e57f6917a6d302374b7b56bebaa3fb
longjun xu <xulongjun.fr@gmail.com>
2023-12-19 15:58:53 +0100

    feat : Add UnitTest for the Adapter design pattern

--- Structural/Adapter/tests/AdapterExample.Tests/AdapterExampleUnitTest.cs
+++ Structural/Adapter/tests/AdapterExample.Tests/AdapterExampleUnitTest.cs
@@ -1,11 +1,38 @@
+using AdapterExample.Adapter;
+
 namespace AdapterExample.Tests
 {
     public class AdapterExampleUnitTest
     {
         [Fact]
-        public void Test1()
+        public void TestEmployeeAdapter_ProcessCompanySalary()
         {
+            // Arrange
+            var adapter = new EmployeeAdapter();
+            string[,] employeesArray = new string[,]
+            {
+                { "1", "John Doe", "Developer", "70000" },
+                { "2", "Jane Smith", "Manager", "90000" }
+            };
 
+            var originalConsoleOut = Console.Out; // Save the original console output stream
+            using var sw = new StringWriter();
+            Console.SetOut(sw); // Redirect console output to StringWriter
+
+            // Act
+            adapter.ProcessCompanySalary(employeesArray);
+
+            // Reset the console output to original
+            Console.SetOut(originalConsoleOut);
+            var result = sw.ToString().Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+
+            // Assert
+            // Verify that the output contains expected lines
+            Assert.Contains(result, line => line.Contains("Rs.70000 Salary Credited to John Doe Account"));
+            Assert.Contains(result, line => line.Contains("Rs.90000 Salary Credited to Jane Smith Account"));
+
+            // Verify console output for Adapter
+            Assert.Contains(result, line => line.Contains("Adapter converted Array of Employee to List of Employee"));
         }
     }
 }
